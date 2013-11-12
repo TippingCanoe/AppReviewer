c0750583f7028f2fab5b8000968927a0715b7e13
Jeff Friesen <j.friesen@tippingcanoe.com>
2013-11-12 08:15:54 -0600

    initial commit

---
+++ AppReviewer/AppReviewer.h
@@ -0,0 +1,13 @@
+//
+//  AppReviewer.h
+//  AppReviewer
+//
+//  Created by Jeff on 11/12/2013.
+//  Copyright (c) 2013 Tipping Canoe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppReviewer : NSObject
+
+@end
---
+++ AppReviewer/AppReviewer.m
@@ -0,0 +1,13 @@
+//
+//  AppReviewer.m
+//  AppReviewer
+//
+//  Created by Jeff on 11/12/2013.
+//  Copyright (c) 2013 Tipping Canoe. All rights reserved.
+//
+
+#import "AppReviewer.h"
+
+@implementation AppReviewer
+
+@end
